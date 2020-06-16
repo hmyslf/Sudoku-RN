dfdd855ae5d950c5cc02ac56140d5c1374e8b69d
hmyslf <yosa.alfq4@gmail.com>
2020-06-16 15:12:19 +0700

    sudoku

--- sudoku-rn/App.js
+++ sudoku-rn/App.js
@@ -1,13 +1,124 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import SudokuGrid from './components/SudokuGrid.js';
+import axios from 'axios';
 
 export default function App() {
+  const [boards, setBoards] = useState([]);
+  const [reset, setReset] = useState(false);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://sugoku.herokuapp.com/board?difficulty=random')
+      .then(({ data }) => {
+        setBoards(data);
+      })
+      .catch(err => {
+        setMessage(err.message);
+        setTimeout(() => {
+          setMessage('')
+        }, 3000)
+      })
+  }, [reset]);
+
+  const encodeSudoku = (params) => {
+    const encodeBoard = (board) => board.reduce((result, row, i) => result + `%5B${encodeURIComponent(row)}%5D${i === board.length -1 ? '' : '%2C'}`, '')
+
+    const encodeParams = (params) => 
+      Object.keys(params)
+      .map(key => key + '=' + `%5B${encodeBoard(params[key])}%5D`)
+      .join('&');
+
+    return encodeParams(params);
+  }
+
+  const solveSudoku = () => {
+    const data = encodeSudoku(boards);
+    axios
+      .post('http://sugoku.herokuapp.com/solve', data)
+      .then(({ data }) => {
+        const solved = {
+          board: data.solution
+        }
+        setBoards(solved);
+        setMessage(data.status);
+        setTimeout(() => {
+          setMessage('');
+        }, 3000)
+      })
+      .catch(err => {
+        setMessage(err.message);
+        setTimeout(() => {
+          setMessage('');
+        }, 3000)
+      });
+  }
+
+  const submitSudoku = () => {
+    const data = encodeSudoku(boards);
+    axios
+      .post('http://sugoku.herokuapp.com/validate', data)
+      .then(({ data }) => {
+        setMessage(data.status);
+        setTimeout(() => {
+          setMessage('');
+        }, 3000);
+      })
+      .catch(err => {
+        setMessage(err.message);
+        setTimeout(() => {
+          setMessage('');
+        }, 3000);
+      });
+  }
+  const resetBoard = () => {
+    setMessage('');
+    setReset(!reset);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sudoku with React Native</Text>
+      <Text>{message}</Text>
+      <View style={styles.container}>
+        <SudokuGrid boards={boards} />
+      </View>
+      <View style={
+        styles.button_rows
+      }>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            styles.greenBg
+          ]}
+          onPress={event => submitSudoku(event)}
+        >
+          <Text style={styles.whiteText}>Submit Sudoku</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            styles.blueBg
+          ]}
+          onPress={event => solveSudoku(event)}
+        >
+          <Text style={styles.whiteText}>Solve Sudoku</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            styles.redBg
+          ]}
+          onPress={event => resetBoard(event)}
+        >
+          <Text style={styles.whiteText}>Reset Board</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -15,5 +126,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50
   },
+  whiteText: {
+    color: "#fff"
+  },
+  button_rows: {
+    flex: 1,
+    paddingTop: 50,
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  button: {
+    height: 40,
+    marginRight: 10,
+    padding: 10,
+    borderRadius: 5
+  },
+  redBg: {
+    backgroundColor: '#f00'
+  },
+  blueBg: {
+    backgroundColor: '#00f'
+  },
+  greenBg: {
+    backgroundColor: '#0f0'
+  }
 });
---
+++ sudoku-rn/components/SudokuGrid.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, FlatList, TextInput, View, Text } from 'react-native';
+
+function SudokuGrid (props) {
+    const { boards } = props;
+    const handleChange = (event, item, index) => {
+        if(event === '') event = 0;
+        item[index] = parseInt(event);
+    }
+    return (
+        <View>
+            <FlatList data={boards.board}
+            renderItem={({ item }) => 
+                <View style={styles.grid}>
+                    {
+                        item.map((el, index) => {
+                            return (
+                                <TextInput
+                                    style={styles.sudokuColumn}
+                                    editable={el ? false : true}
+                                    key={index.toString()}
+                                    defaultValue={el.toString()}
+                                    maxLength={1}
+                                    keyboardType={'numeric'}
+                                    onChangeText={event => handleChange(event, item, index)}
+                                />
+                            )
+                        })
+                    }
+                </View>
+                }
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    grid: {
+        flex: 0.2,
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+    sudokuColumn: {
+        backgroundColor: "black",
+        padding: 5,
+        color: "white",
+        justifyContent: "center",
+        textAlign: "center",
+        height: 50
+    }
+});
+
+export default SudokuGrid;
